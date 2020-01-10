51554636e24b1b4dc40293cea5a0c157da2b6c4d
sjung7674-ncomz <sjung7674>
2020-01-10 17:22:01 +0900

    코드정렬

--- MergeSort.java
+++ MergeSort.java
@@ -1,41 +1,43 @@
-/** 
+/**
  * 합병정렬 시간 복잡도 : O(nlogn)
  */
 
-public class MergeSort{
-    public void sort(int[] arr, int start, int end){
-        if(start<end){
-            int mid = (start+end)/2;
-            sort(arr,start,mid);
-            sort(arr,mid+1,end);
+public class MergeSort {
+    public void sort(int[] arr, int start, int end) {
+        if (start < end) {
+            int mid = (start + end) / 2;
+            sort(arr, start, mid);
+            sort(arr, mid + 1, end);
             merge(arr, start, end, mid);
         }
     }
-    public void merge(int[] arr, int start, int end, int mid){
-        System.out.println(start+" "+mid+" "+end);
+
+    public void merge(int[] arr, int start, int end, int mid) {
+        System.out.println(start + " " + mid + " " + end);
         int i = start;
-        int j = mid +1;
+        int j = mid + 1;
         int k = start;
-        int [] sort = new int[arr.length];
-        while(i <= mid && j <= end){
-            if(arr[i]<=arr[j]){
+        int[] sort = new int[arr.length];
+        while (i <= mid && j <= end) {
+            if (arr[i] <= arr[j]) {
                 sort[k++] = arr[i++];
-            }else{
+            } else {
                 sort[k++] = arr[j++];
             }
         }
-        while(i<=mid){
+        while (i <= mid) {
             sort[k++] = arr[i++];
         }
-        while(j<=end){
+        while (j <= end) {
             sort[k++] = arr[j++];
         }
-        for(int l = start; l<=end ; l++){
+        for (int l = start; l <= end; l++) {
             arr[l] = sort[l];
         }
     }
+
     public static void main(String[] args) {
-        int[] arr = { 5, 3, 8, 9, 4, 1, 6, 2, 7,1,1,1,1,1,1,1};
+        int[] arr = { 5, 3, 8, 9, 4, 1, 6, 2, 7, 1, 1, 1, 1, 1, 1, 1 };
         MergeSort sort = new MergeSort();
         sort.sort(arr, 0, arr.length - 1);
 
